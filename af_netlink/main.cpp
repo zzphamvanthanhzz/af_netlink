94409afbbdfeced71755b9eb7fa822321ac79b6d
thanhpv <thanhpv@vng.com.vn>
2015-10-20 21:33:44 +0700

    Init af_netlink

---
+++ af_netlink/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: root
+ *
+ * Created on October 20, 2015, 9:32 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
