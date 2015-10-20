e4f968e28be0f4ceb4f911f245a13687e56e1eae
thanhpv <thanhpv@vng.com.vn>
2015-10-20 21:59:25 +0700

    add main

---
+++ af_netlink/src/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: thanhpv
+ *
+ * Created on October 20, 2015, 9:58 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
