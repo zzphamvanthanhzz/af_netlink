211fea88fb950786d20b4da51d7df09a11733894
thanhpv <thanhpv@vng.com.vn>
2015-10-21 17:13:25 +0700

    complete as ss

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Using af_netlink to get kernel info
 
 Git doesn't allow commit empty folder
+
+REFERENCE:
+https://github.com/kristrev/inet-diag-example/blob/master/inet_monitor.c
--- af_netlink/src/main.cpp
+++ af_netlink/src/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <cstdlib>
+#include <unistd.h>
 #include <string.h>//for memset
 #include <linux/inet_diag.h>
 #include <linux/netlink.h>
@@ -13,13 +14,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <errno.h>
 
-#include "common.h"
+#include "inet_diag.h"
+#include "netlink.h"
+#include "rtnetlink.h"
+#include "sock_diag.h"
+#include "socket.h"
+#include "ss_common.h"
+#include "unix_diag.h"
 
-#define TCPF_ALL 0xFFF
+
 #define SOCKET_BUFFER_SIZE (getpagesize() < 8192L ? getpagesize() : 8192L)
-
+#define SS_ALL ((1<<SS_MAX)-1)
 using namespace std;
+static int tcpConnEstaplished, tcpConnFinWait, tcpConnTimeWait, tcpConnCloseWait;
 
 /*
  * 
@@ -32,48 +41,150 @@ void showbits(unsigned int x) {
 	printf("\n");
 }
 
-bool parse_msg_diag(struct inet_diag_msg *diag_msg, int rtalen) {
+int sockdiag_send(int family, int fd, int protocol, struct filter *f) {
+	struct sockaddr_nl nladdr;
 
+	struct {
+		struct nlmsghdr nlh;
+		struct inet_diag_req_v2 r;
+	} req;
+	struct msghdr msg;
+	struct iovec iov[3];
+
+
+	memset(&nladdr, 0, sizeof (nladdr));
+	nladdr.nl_family = AF_NETLINK;
+
+	req.nlh.nlmsg_len = sizeof (req);
+	req.nlh.nlmsg_type = SOCK_DIAG_BY_FAMILY;
+	req.nlh.nlmsg_flags = NLM_F_ROOT | NLM_F_MATCH | NLM_F_REQUEST;
+	req.nlh.nlmsg_pid = 0;
+	req.nlh.nlmsg_seq = 123456;
+	memset(&req.r, 0, sizeof (req.r));
+	req.r.sdiag_family = family;
+	req.r.sdiag_protocol = protocol;
+	req.r.idiag_states = f->states;
+
+	iov[0] = (struct iovec){&req, sizeof (req)};
+
+	msg = (struct msghdr){&nladdr, sizeof (nladdr), iov, f->f ? 3 : 1, NULL, 0, 0};
+
+	if (sendmsg(fd, &msg, 0) < 0) {
+		printf("Error in sendmsg %s", strerror(errno));
+		return -1;
+	}
+	return 0;
 }
 
-bool send_msg_diag(int sockfd) {
+bool GetTCPState(int32_t family, int32_t fd) {
+
+	int32_t protocol = IPPROTO_TCP;
+	struct filter current_filter = (struct filter){~0, SS_ALL & ~((1 << SS_LISTEN) | (1 << SS_CLOSE) | (1 << SS_SYN_RECV)), (1 << AF_INET) | (1 << AF_INET6), NULL};
+	int32_t send_af_sts = sockdiag_send(family, fd, protocol, &current_filter);
+	if (send_af_sts) {
+		printf("Error inside sockdiag_send %s", strerror(errno));
+		return false;
+	}
+
+	struct iovec iov[3];
+	char buf[SOCKET_BUFFER_SIZE];
+	struct sockaddr_nl nladdr;
 	struct msghdr msg;
-	struct nlmsghdr nlh;
-	struct inet_diag_req_v2 conn_req;
-	struct sockaddr_nl sa;
-	struct iovec iov[4];
-	int retval = 0;
+	memset(&nladdr, 0, sizeof (nladdr));
+	nladdr.nl_family = family;
+	iov[0] = (struct iovec){buf, sizeof (buf)};
+	while (1) {
+		int status;
+		struct nlmsghdr *h;
+		msg = (struct msghdr){
+			(void*) &nladdr, sizeof (nladdr),
+			iov, 1,
+			NULL, 0,
+			0
+		};
 
-	//Filter
-	struct rtattr rta;
-	void *filter_mem = NULL;
-	int filter_len = 0;
+		status = recvmsg(fd, &msg, 0);
+		if (status < 0) {
+			if (errno == EINTR)
+				continue;
+			printf("OVERRUN\n");
+			continue;
+		}
+		if (status == 0) {
+			//printf("EOF on netlink\n");
+			return true;
+		}
+		h = (struct nlmsghdr*) buf;
+		while (NLMSG_OK(h, status)) {
+			struct inet_diag_msg *r = (struct inet_diag_msg *) NLMSG_DATA(h);
 
-	memset(&msg, 0, sizeof (msg));
-	memset(&sa, 0, sizeof (sa));
-	memset(&nlh, 0, sizeof (nlh));
-	memset(&conn_req, 0, sizeof (conn_req));
 
-	//init sockaddr_nl
-	//sa.nl_pid = 0 means kernel will receive message
-	sa.nl_family = AF_NETLINK;
+			if (h->nlmsg_seq != 123456) {
+				h = NLMSG_NEXT(h, status);
+				continue;
+			}
+			if (h->nlmsg_type == NLMSG_DONE) {
+				if (family == PF_INET) {
+					printf("Back to ipv6\n");
+					family = PF_INET6;
+					GetTCPState(family, fd);
+					return true;
+				} else {
+					return true;
+				}
+			}
+			if (h->nlmsg_type == NLMSG_ERROR) {
+				struct nlmsgerr *err = (struct nlmsgerr*) NLMSG_DATA(h);
+				if (h->nlmsg_len < NLMSG_LENGTH(sizeof (struct nlmsgerr))) {
+					printf("Message truncated\n");
+				} else {
+					errno = -err->error;
+					printf("Error in message %s\n", strerror(errno));
+					return false;
+				}
+				return true;
+			}
 
-	//Sepcify address family and protocol 
-	//can be UDP, DCCP, Unix Socket
-	conn_req.sdiag_family = AF_INET; //IPv4
-	conn_req.sdiag_protocol = IPPROTO_TCP;
-
-	//Filter only some states:
-	//1 << TCP_SYN_RECV: left shift 1 TCP_SYN_RECV bits
-	//1 >> TCP_SYN_RECV: right shift 1 TCP_SYN_RECV bits
-	conn_req.idiag_states = TCPF_ALL & ~((1 << TCP_SYN_RECV) | (1 << TCP_TIME_WAIT) | (1 << TCP_CLOSE));
+			//			if (r->idiag_family == PF_INET6 || r->idiag_family == PF_INET)
+			switch (r->idiag_state) {
+				case 1:
+					tcpConnEstaplished++;
+					break;
+				case 4:
+				case 5:
+					tcpConnFinWait++;
+					break;
+				case 6:
+					tcpConnTimeWait++;
+					break;
+				case 8:
+					tcpConnCloseWait++;
+					break;
+			}
+			h = NLMSG_NEXT(h, status);
+		}
+	}
+	return true;
 }
 
 int main(int argc, char** argv) {
-	showbits(~((1 << TCP_SYN_RECV) | (1 << TCP_TIME_WAIT) | (1 << TCP_CLOSE)));
-	showbits(((1 << TCP_SYN_RECV) | (1 << TCP_TIME_WAIT) | (1 << TCP_CLOSE)));
-	showbits(TCPF_ALL & ~((1 << TCP_SYN_RECV) | (1 << TCP_TIME_WAIT) | (1 << TCP_CLOSE)));
-	send_msg_diag(9);
+	while (1) {
+		tcpConnEstaplished = tcpConnFinWait = tcpConnTimeWait = tcpConnCloseWait = 0;
+		int family = PF_INET;
+		int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_INET_DIAG);
+		if (fd < 0) {
+			printf("Error open netlink socket: %s\n", strerror(errno));
+			return false;
+		}
+		bool rs = GetTCPState(family, fd);
+		//	printf("est: %d\n twait: %d \n cwait: %d \n fwait: %d\n", _networkstat->tcpConnEstaplished,
+		//			_networkstat->tcpConnTimeWait, _networkstat->tcpConnCloseWait, _networkstat->tcpConnFinWait);
+		close(fd);
+
+		printf("tcpConnEstaplished: %d\t tcpConnFinWait:%d\t tcpConnTimeWait:%d\t  tcpConnCloseWait: %d\n",
+				tcpConnEstaplished, tcpConnFinWait, tcpConnTimeWait, tcpConnCloseWait);
+		sleep(1);
+	}
 	return 0;
 }
 
